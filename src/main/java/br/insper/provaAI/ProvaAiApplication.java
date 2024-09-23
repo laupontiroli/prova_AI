8d7de51bac3669e4878fb65a5ec48ea491ead73f
laupontiroli <laurapm@al.insper.edu.br>
2024-09-23 09:12:06 -0300

    adicionando arquivos

---
+++ src/main/java/br/insper/provaAI/ProvaAiApplication.java
@@ -0,0 +1,13 @@
+package br.insper.provaAI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProvaAiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProvaAiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/insper/provaAI/ProvaAiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.insper.provaAI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProvaAiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
